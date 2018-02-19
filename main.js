d016625efaa870e54f592f3fd3d56aedbc76f39c
Dragoness390 <cera11@hotmail.se>
2018-02-19 23:21:25 +0100

    In progress, done titles, added item yourself to done list, done items will be green, added buttons remove latest item and remove all items

--- main.js
+++ main.js
@@ -1,25 +1,62 @@
 function todoList() {
     var item = document.getElementById("todoInput").value
     var text = document.createTextNode(item)
-    var newItem = document.createElement("li")
+    var todoList = document.getElementById("todoList");  //getting element <ul> to add element to
+    var newItem = document.createElement("li")  //creating li element to add
+    
     newItem.appendChild(text)
     document.getElementById("todoList").appendChild(newItem)
-}
 
-/* varje gång du klickar på ett <li>-element tas de bort från sidan.*/
-
-
-
-const listItems = document.querySelectorAll('li');  /* välj alla li taggar*/      
-
-for(const item of listItems){
-    item.addEventListener('click', function(){  /* vid klick */
-        this.parentElement.removeChild(this);  /* ta bort båda föräldrar, UL + barn elementet, li */ 
+    const todo = document.getElementById('todoList');/* Hämta elementet med id todo*/
+const done = document.getElementById('done') /* Hämta elementet med id done*/
+const listItems = document.querySelectorAll('li'); /* välj alla li taggar*/
+done.style.color='forestgreen'; /* gör så att när den är väl checkad så kommer den till done listan och texten blir grön */
+   
+ 
+  newItem.onclick = function() {
+   
+      for(const item of listItems){
+    item.addEventListener('click', function(){  /* vid click*/
+        todo.removeChild(this);         /* ta bort todo barnet*/
+        done.appendChild(this);         /* skapas istället under done*/
     })
 }
+  }
+  if (todoList.childElementCount == 0) {  //using if/else statement to add items to top of list
+    todoList.appendChild(newItem); // will add if count of ul children is 0 otherwise add before first item
+  }
+  else {
+    todoList.insertBefore(newItem, todoList.firstChild);
+  }
+}
+
+
+
+/* remove items NYTT */
+function removeElementTodoList() {
+  
+ 
+    document.body.removeChild(document.body.todoList.li.lastElementChild);
+   
+ 
+}
+
+/* remove NYTT Slut */
+
+
+
+
+
+
+
 
 
 /* NYTT */
+
+//Övning 6 - Stryk mig inte, flytta mig tack
+/*Använd samma HTML som i förra övningen. Du ska istället för att ta bort elementet när du klickar på det, flytta det till listan med id="done". Så vid klick så tas det bort från första listan och läggs i den andra listan.*/
+
+
 
 
 
@@ -51,3 +88,4 @@ function addItem() {
   }
 }
 /* NYTT 2 SLUTAR */
+
